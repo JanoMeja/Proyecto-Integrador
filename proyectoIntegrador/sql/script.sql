332976bbb7f5e797932d8d97fb833157461b468b
aguskrmn <acarman@udesa.edu.ar>
2023-04-10 22:08:03 -0300

    las tres tablas

--- proyectoIntegrador/sql/script.sql
+++ proyectoIntegrador/sql/script.sql
@@ -1,7 +1,43 @@
 /* tabla usuarios */
+create table usuarios (
+-- nombre        tipo         restriccion
+id               int         unsigned primary key  auto_increment,
+email            varchar(200)    not null,
+contraseña       varchar(150)   not null,
+foto_de_perfil   varchar(500)   not null,
+fecha            date,
+dni              int            unsigned unique not null,
+cratedAT TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+updatedAT TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
 
 
 /* tabla productos */
+create table productos (
+-- nombre        tipo         restriccion          
+id              int           unsigned primary key  auto_increment,
+user_id         int           unsigned not null,  
+nombre          varchar(250)  not null,
+descrpcion      varchar(2000)  not null,
+
+FOREIGN KEY (user_id)  REFERENCES usuarios(id),
+
+cratedAT TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+updatedAT TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
 
 
 /* tabla comentarios */
+create table comentarios (
+-- nombre        tipo         restriccion
+id               int          unsigned primary key auto_increment,
+id_post          int          unsigned not null,
+id_usuario       int          unsigned not null,
+comentarios      varchar(5000) not null, 
+
+FOREIGN KEY (id_usuario)  REFERENCES usuarios(id),
+FOREIGN KEY (id_post)  REFERENCES productos(id),
+
+cratedAT TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+updatedAT TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
