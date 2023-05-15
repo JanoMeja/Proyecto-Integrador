28402fc1677b03f6e97051a32c79ce37995c51cf
esuarez2004 <esuarez@udesa.edu.ar>
2023-05-15 17:59:43 -0300

    modelos

---
+++ proyectoIntegrador/database/models/Comentarios.js
@@ -0,0 +1,36 @@
+module.exports = function (sequilize, dataTypes) {
+    //creo un alias para que sequelize sepa con cual modelo debe conectar */
+    let alias = "Comentario";
+    // creo una variable (Cols) con la estructura de la tabla
+    let cols  = {
+        id:{
+            AutoIncrement : true,
+            primaryKey: true,
+            type:dataTypes.INTEGER
+        },
+        idPost:{
+            type:dataTypes.INTEGER
+        },
+        idUsuario:{
+            type:dataTypes.INTEGER
+        },
+        comentarios:{
+            type:dataTypes.STRING
+        },
+        createdAT:{
+            type: dataTypes.DATE,
+            allownull: true
+        },
+        updatedAT: {
+            type: dataTypes.DATE,
+            allownull: true
+        }
+    }
+
+//creo un OBJ con la configuracion de la tabla
+    let config = {
+        tableName: "comentarios",
+        timeStamp: false,
+
+    } 
+}
